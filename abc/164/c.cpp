9deef1f54ca34c415fa9ef4e3d8da2659c27a173
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-04-27 18:51:24 +0900

    abc164_c

--- abc/164/c.cpp
+++ abc/164/c.cpp
@@ -2,26 +2,6 @@
 
 using namespace std;
 
-#define deg_to_rad(deg) (((deg) / 360) * 2 * M_PI)
-#define rad_to_deg(rad) (((rad) / 2 / M_PI) * 360)
-
-typedef long long ll;
-typedef long double ld;
-
-typedef vector<int> vi;
-typedef vector<ld> vd;
-typedef vector<ll> vl;
-typedef vector<string> vs;
-
-typedef vector<vector<int>> vvi;
-typedef vector<vector<ld>> vvd;
-typedef vector<vector<ll>> vvl;
-
-typedef vector<vector<vector<int>>> vvvi;
-typedef vector<vector<vector<ld>>> vvvd;
-typedef vector<vector<vector<ll>>> vvvl;
-
-constexpr int MOD = 1000000007;
 constexpr char nl = '\n';
 
 int main() {
@@ -31,12 +11,12 @@ int main() {
 
   int N;
   cin >> N;
-  map<string, int> S;
+  set<string> S;
 
   for (int i = 0; i < N; i++) {
     string s;
     cin >> s;
-    S[s] = 1;
+    S.insert(s);
   }
 
   cout << S.size() << nl;
