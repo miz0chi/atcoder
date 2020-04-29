23375715d26f15844c1a93401c192cf3b4e226fb
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-04-29 15:51:41 +0900

    submit abc163_d

--- abc/163/d.cpp
+++ abc/163/d.cpp
@@ -2,18 +2,25 @@
 
 using namespace std;
 
-int main() {
-  cin.tie(0);
-  ios::sync_with_stdio(false);
+typedef long long ll;
 
-  long long N, K;
+constexpr int MOD = 1000000007;
+constexpr char nl = '\n';
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int N, K;
   cin >> N >> K;
-  long long sum = 0;
-  for (long long k = K; k <= N + 1; k++) {
-    long long min_sum = (k - 1) * k / 2;
-    long long max_sum = (N - (k - 1) + N) * k / 2;
+  ll sum = 0;
+  for (ll k = K; k <= N + 1; k++) {
+    ll min_sum = (k - 1) * k / 2;
+    ll max_sum = (N - (k - 1) + N) * k / 2;
     sum += max_sum - min_sum + 1;
-    // cout << max_sum << ":" << min_sum << endl;
+    // cerr << max_sum << ":" << min_sum << nl;
   }
-  cout << sum % 1000000007 << endl;
+  cout << sum % MOD << nl;
+  return 0;
 }
