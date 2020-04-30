58c14fb1decd677f3c612d525408b02e155c5d03
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-04-30 14:49:04 +0900

    submit abc106_b

---
+++ abc/106/b.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int N;
+  cin >> N;
+
+  int ans = 0;
+  for (int i = 1; i <= N; i += 2) {
+    int sum = 0;
+    for (int j = 1; j <= i; j++) {
+      if (N % j == 0) {
+        sum++;
+      }
+    }
+    if (sum == 8) {
+      ans++;
+    }
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
