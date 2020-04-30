7078d614f6eb2bcd7f1f5260c9bbe47a170d00da
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-04-30 14:51:58 +0900

    submit abc106_b

--- abc/106/b.cpp
+++ abc/106/b.cpp
@@ -14,7 +14,7 @@ int main() {
   for (int i = 1; i <= N; i += 2) {
     int sum = 0;
     for (int j = 1; j <= i; j++) {
-      if (N % j == 0) {
+      if (i % j == 0) {
         sum++;
       }
     }
