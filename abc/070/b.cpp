abb2f05931c5b5c46df90db7ce66f1f89bca1adc
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-02 15:17:12 +0900

    submit abc070_b

---
+++ abc/070/b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int A, B, C, D;
+  cin >> A >> B >> C >> D;
+
+  int t = 0;
+  if (B > C || A > D) {
+    t = min(B, D) - max(A, C);
+  }
+
+  cout << t << '\n';
+
+  return 0;
+}
