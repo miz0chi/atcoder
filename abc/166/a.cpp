0ddabb41e9d819af44a2ede377dc42ca575e867c
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-03 21:01:56 +0900

    submit abc166_a

---
+++ abc/166/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  string S;
+  cin >> S;
+
+  if (S == "ABC") {
+    cout << "ARC" << '\n';
+  } else {
+    cout << "ABC" << '\n';
+  }
+
+  return 0;
+}
