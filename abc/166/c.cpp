8bc773bb19257ae995b820c925ad0c46d011a11a
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-03 22:52:38 +0900

    submit abc166_c

---
+++ abc/166/c.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using Graph = vector<vector<int>>;
+
+vector<bool> seen, hb;
+void dfs(const Graph &G, const vector<int64_t> &h, int v) {
+  seen[v] = true;
+
+  for (auto next_v : G[v]) {
+    if (h[v] <= h[next_v]) hb[v] = false;
+    if (seen[next_v]) continue;
+    dfs(G, h, next_v);
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int n, m;
+  cin >> n >> m;
+
+  vector<int64_t> h(n);
+  for (int i = 0; i < n; i++) {
+    cin >> h[i];
+  }
+
+  Graph r(n);
+  for (int i = 0; i < m; i++) {
+    int a, b;
+    cin >> a >> b;
+    r[a - 1].push_back(b - 1);
+    r[b - 1].push_back(a - 1);
+  }
+
+  seen.assign(n, false);
+  hb.assign(n, true);
+  dfs(r, h, 0);
+
+  int sum = 0;
+  for (int i = 0; i < n; i++) {
+    if (hb[i] == true) sum++;
+    cerr << hb[i] << '\n';
+  }
+
+  cout << sum << '\n';
+
+  return 0;
+}
