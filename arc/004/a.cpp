65705e7e4812c76aec0c995f38c8d569052f7a09
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-04 21:38:40 +0900

    arc004_a

---
+++ arc/004/a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int n;
+  cin >> n;
+
+  vector<pair<double, double>> a(n);
+
+  for (int i = 0; i < n; i++) {
+    cin >> a[i].first >> a[i].second;
+  }
+
+  double ans = 0.0;
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = i + 1; j < n; j++) {
+      double dist = sqrt(pow(a[i].first - a[j].first, 2) +
+                         pow(a[i].second - a[j].second, 2));
+      ans = max(ans, dist);
+    }
+  }
+
+  cout << ans << '\n';
+
+  return 0;
+}
