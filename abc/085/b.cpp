52604a398b99f12f337f1dbfcba176a5569887f3
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-05 22:06:54 +0900

    submit abc085_b

--- abc/085/b.cpp
+++ abc/085/b.cpp
@@ -2,28 +2,6 @@
 
 using namespace std;
 
-#define deg_to_rad(deg) (((deg) / 360) * 2 * M_PI)
-#define rad_to_deg(rad) (((rad) / 2 / M_PI) * 360)
-
-typedef long long ll;
-typedef long double ld;
-
-typedef vector<int> vi;
-typedef vector<ld> vd;
-typedef vector<ll> vl;
-typedef vector<string> vs;
-
-typedef vector<vector<int>> vvi;
-typedef vector<vector<ld>> vvd;
-typedef vector<vector<ll>> vvl;
-
-typedef vector<vector<vector<int>>> vvvi;
-typedef vector<vector<vector<ld>>> vvvd;
-typedef vector<vector<vector<ll>>> vvvl;
-
-constexpr int MOD = 1000000007;
-constexpr char nl = '\n';
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -31,7 +9,7 @@ int main() {
 
   int N;
   cin >> N;
-  vi d(N);
+  vector<int> d(N);
   for (int i = 0; i < N; i++) {
     cin >> d.at(i);
   }
@@ -42,8 +20,7 @@ int main() {
     if (d.at(i) < d.at(i - 1)) {
       sum++;
     }
-    // cout << d.at(i) << nl;
   }
 
-  cout << sum << nl;
+  cout << sum << '\n';
 }
