1cee94fd16996ad196087a3ea5090fa8194ad67e
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-05 22:03:44 +0900

    submit abc085_c

--- abc/085/c.cpp
+++ abc/085/c.cpp
@@ -2,28 +2,6 @@
 
 using namespace std;
 
-#define deg_to_rad(deg) (((deg) / 360) * 2 * M_PI)
-#define rad_to_deg(rad) (((rad) / 2 / M_PI) * 360)
-
-typedef long long ll;
-typedef long double ld;
-
-typedef vector<int> vi;
-typedef vector<ld> vd;
-typedef vector<ll> vl;
-typedef vector<string> vs;
-
-typedef vector<vector<int>> vvi;
-typedef vector<vector<ld>> vvd;
-typedef vector<vector<ll>> vvl;
-
-typedef vector<vector<vector<int>>> vvvi;
-typedef vector<vector<vector<ld>>> vvvd;
-typedef vector<vector<vector<ll>>> vvvl;
-
-constexpr int MOD = 1000000007;
-constexpr char nl = '\n';
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -36,10 +14,12 @@ int main() {
     for (int j = 0; j + i <= N; j++) {
       int k = N - i - j;
       if ((i * 10000) + (j * 5000) + (k * 1000) == Y) {
-        cout << i << " " << j << " " << k << nl;
+        cout << i << " "s << j << " "s << k << '\n';
         return 0;
       }
     }
   }
-  cout << -1 << " " << -1 << " " << -1 << nl;
+  cout << -1 << " "s << -1 << " "s << -1 << '\n';
+
+  return 0;
 }
