3cce316f1d11aba5226b2ecbf929a371ff4f4604
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-07 11:38:08 +0900

    submit abc003_a

---
+++ abc/003/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int n;
+  cin >> n;
+
+  cout << (1 + n) * 5000 << '\n';
+
+  return 0;
+}
