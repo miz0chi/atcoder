a42ce405219a19c86d5e26ea44423bd8c0ce0378
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-07 16:59:43 +0900

    submit abc136_b

---
+++ abc/136/b.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int n;
+  cin >> n;
+
+  int64_t sum = 0;
+
+  for (int64_t i = 1; i <= n; ++i) {
+    int cnt = 0;
+    int64_t j = i;
+    while (j) {
+      j /= 10;
+      ++cnt;
+    }
+    if (cnt % 2 == 1) {
+      ++sum;
+    }
+  }
+  cout << sum << '\n';
+
+  return 0;
+}
