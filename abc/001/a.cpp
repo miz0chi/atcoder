cec876a98daa59833813699f2531118748184f08
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-07 10:12:01 +0900

    submit abc001_a

---
+++ abc/001/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int a, b;
+  cin >> a >> b;
+  cout << a - b << '\n';
+
+  return 0;
+}
