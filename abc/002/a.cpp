291e3c21ff971d9a069e43cd23ffffbe52cb5f7b
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-07 10:54:56 +0900

    submit abc002_a

---
+++ abc/002/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int x, y;
+  cin >> x >> y;
+
+  cout << max(x, y) << '\n';
+
+  return 0;
+}
