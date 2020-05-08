4ffb5da14efdfefb5a21f6cb584b1bf517172276
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-08 14:22:38 +0900

    submit abc128_c

---
+++ abc/128/c.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  // input 1
+  int n, m;
+  cin >> n >> m;
+  vector<vector<int>> ms(m);
+  // input 2
+  for (int i = 0; i < m; ++i) {
+    int x;
+    cin >> x;
+    for (int j = 0; j < x; ++j) {
+      int y;
+      cin >> y;
+      ms[i].push_back(y - 1);
+    }
+  }
+  // input 3
+  vector<int> p(m);
+  for (int i = 0; i < m; ++i) {
+    cin >> p[i];
+  }
+
+  int ans = 0;
+  for (int i = 0; i < (1 << n); ++i) {
+    bool ok = true;
+    for (int j = 0; j < m; ++j) {
+      int x = 0;
+      for (int k : ms[j]) {
+        if ((i >> k) & 1) {
+          x ^= 1;
+        }
+      }
+      if (x != p[j]) {
+        ok = false;
+      }
+    }
+    if (ok) {
+      ++ans;
+    }
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
