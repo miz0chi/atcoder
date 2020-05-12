8452610dfc9389a96e6b50f11cc65450bc8de2c7
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-12 20:49:44 +0900

    submit abc167_d

--- abc/167/d.cpp
+++ abc/167/d.cpp
@@ -7,35 +7,53 @@ int main() {
   cin.tie(nullptr);
   cout << fixed << setprecision(15);
 
-  int64_t n, k;
+  int n;
+  int64_t k;
   cin >> n >> k;
-
-  vector<int64_t> a(n);
-  for (int64_t i = 0; i < n; ++i) {
+  vector<int> a(n);
+  for (int i = 0; i < n; ++i) {
     cin >> a[i];
-    --a[i];
   }
 
-  unordered_set<int64_t> c;
-  int64_t goal = 0;
-  c.insert(goal);
-  cerr << goal << '\n';
-  bool ng = true;
-  for (int64_t i = 1; i <= k; ++i) {
-    goal = a[goal];
-    c.insert(goal);
-    cerr << goal << '\n';
-    auto itc = c.find(goal);
-    if (*itc && ng) {
-      int64_t loop = distance(itc, c.end());
-      cerr << *itc << " " << loop << '\n';
-      k %= loop;
-      i = 0;
-      ng = false;
-    }
-  }
+  vector<int> s;
+  vector<int> ord(n + 1, -1);
 
-  cout << goal + 1 << '\n';
+  int c = 1, l = 0;
+  int v = 1;
+  while (ord[v] == -1) {
+    cerr << v << " ";
+    ord[v] = s.size();
+    s.push_back(v);
+    v = a[v - 1];
+  }
+  c = s.size() - ord[v];
+  l = ord[v];
+
+  if (k < l) {
+    cout << s[k] << '\n';
+  } else {
+    k -= l;
+    k %= c;
+    cout << s[l + k] << '\n';
+  }
+  // unordered_set<int64_t> c;
+  // int64_t goal = 0;
+  // c.insert(goal);
+  // cerr << goal << '\n';
+  // for (int64_t i = 1; i <= k; ++i) {
+  //   goal = a[goal];
+  //   auto itc = c.find(goal);
+  //   if (itc == c.end()) {
+  //     c.insert(goal);
+  //     cerr << goal << '\n';
+  //   } else {
+  //     int64_t loop = distance(itc, c.end());
+  //     cerr << *itc << " " << loop << '\n';
+  //     int64_t kk = (k - i) % loop;
+  //     cout << *next(itc, kk) + 1 << '\n';
+  //     break;
+  //   }
+  // }
 
   return 0;
 }
