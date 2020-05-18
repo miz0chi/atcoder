fa87b4240184f9701a72998bd2513c4856436ec7
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-18 17:41:14 +0900

    submit abc168_c

--- abc/168/c.cpp
+++ abc/168/c.cpp
@@ -2,34 +2,31 @@
 
 using namespace std;
 
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define deg_to_rad(deg) (((deg) / 360) * 2 * M_PI)
-#define rad_to_deg(rad) (((rad) / 2 / M_PI) * 360)
+// #define deg_to_rad(deg) (((deg) / 360) * 2 * M_PI)
+// #define rad_to_deg(rad) (((rad) / 2 / M_PI) * 360)
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  cout << fixed << setprecision(15);
+  cout << fixed << setprecision(20);
 
   int a, b, h, m;
   cin >> a >> b >> h >> m;
 
-  double mang, hang, ang, ans;
-  mang = m * 360.0 / 60.0;
-  hang = h * 360.0 / 12.0;
-  hang += m * 360.0 / 720.0;
+  double rad = M_PI * 2 * ((double)h / 12 + (double)m / 720 - (double)m / 60);
 
-  mang = mang;
-  hang = hang;
+  // double mang, hang, ang, ans;
+  // mang = m * 360.0 / 60.0;
+  // hang = h * 360.0 / 12.0;
+  // hang += m * 360.0 / 720.0;
 
-  ang = max(mang, hang) - min(mang, hang);
-  if (ang > 180) {
-    ang = 360 - ang;
-  }
-  ang = deg_to_rad(ang);
+  // ang = max(mang, hang) - min(mang, hang);
+  // if (ang > 180) {
+  //   ang = 360 - ang;
+  // }
+  // ang = deg_to_rad(ang);
 
-  ans = sqrt((a * a) + (b * b) - (2 * a * b * cos(ang)));
-
+  double ans = sqrt((double)(a * a + b * b) - (double)(2 * a * b) * cos(rad));
   cout << ans << '\n';
 
   return 0;
