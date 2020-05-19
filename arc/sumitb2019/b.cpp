40aaaddf834a6b2f77b996ffea4bdbdc7b9da838
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-19 16:16:34 +0900

    submit arcsumitb2019_b

---
+++ arc/sumitb2019/b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  double n;
+  cin >> n;
+
+  int x = (n + (1.08 - 0.01)) / 1.08;
+  if ((int)n == (int)(x * 1.08)) {
+    cout << x << '\n';
+  } else {
+    cout << ":(" << '\n';
+  }
+
+  return 0;
+}
