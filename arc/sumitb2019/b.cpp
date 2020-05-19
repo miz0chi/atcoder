7e41b986b70144437be0bd398036de5ab19ef152
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-19 16:20:13 +0900

    submit arcsumitb2019_b

--- arc/sumitb2019/b.cpp
+++ arc/sumitb2019/b.cpp
@@ -7,11 +7,11 @@ int main() {
   cin.tie(nullptr);
   cout << fixed << setprecision(15);
 
-  double n;
+  int n;
   cin >> n;
 
   int x = (n + (1.08 - 0.01)) / 1.08;
-  if ((int)n == (int)(x * 1.08)) {
+  if (n == (int)(x * 1.08)) {
     cout << x << '\n';
   } else {
     cout << ":(" << '\n';
