80ee6501c56c387ba153a780fbb5232442cec47c
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-23 16:57:15 +0900

    submit atcodercontent_f

---
+++ content/f.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int n;
+  cin >> n;
+  vector<vector<char>> a(n, vector<char>(n));
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      cin >> a[i][j];
+    }
+  }
+  string ans(n, 's');
+  int i = 0;
+  while (i < n - 1) {
+    bool ok = false;
+    for (int j = 0; j < n; ++j) {
+      for (int k = 0; k < n; ++k) {
+        if (a[i][j] == a[n - 1][k]) {
+          ans[i] = a[i][j];
+          ans[n - 1] = a[n - 1][k];
+          ok = true;
+          break;
+        }
+      }
+      if (ok) {
+        break;
+      }
+    }
+    if (!ok) {
+      cout << -1 << '\n';
+      return 0;
+    }
+    ++i;
+    --n;
+  }
+  cout << ans << '\n';
+
+  return 0;
+}
