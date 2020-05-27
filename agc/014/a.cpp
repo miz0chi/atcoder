36aee0f9b1d20cea731dde24c9aaf3e3b429656d
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-27 21:11:02 +0900

    submit agc014_a

---
+++ agc/014/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int64_t a, b, c;
+  cin >> a >> b >> c;
+
+  int64_t a0 = a, b0 = b, c0 = c;
+  int i = 0;
+  while (a % 2 != 1 && b % 2 != 1 && c % 2 != 1) {
+    ++i;
+    int64_t bc = (b + c) / 2, ca = (c + a) / 2, ab = (a + b) / 2;
+    a = bc;
+    b = ca;
+    c = ab;
+    if (a == a0 && b == b0 && c == c0) {
+      cout << -1 << '\n';
+      return 0;
+    }
+  }
+  cout << i << '\n';
+
+  return 0;
+}
