c054b8472d74d85cf8ece1efa6a441954d7a1865
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-30 21:32:04 +0900

    submit arcnomura2020_b

--- arc/nomura2020/b.cpp
+++ arc/nomura2020/b.cpp
@@ -12,20 +12,28 @@ int main() {
   string pd = t, d = t;
   int len = t.size();
 
-  bool ok = false;
+  // bool ok = false;
+  // for (int i = 0; i < len; ++i) {
+  //   if (t[i] == 'P') {
+  //     ok = true;
+  //   } else if (t[i] == '?') {
+  //     if (ok) {
+  //       t[i] = 'D';
+  //       ok = false;
+  //     } else {
+  //       t[i] = 'P';
+  //       ok = true;
+  //     }
+  //   } else {
+  //     ok = false;
+  //   }
+  //   cout << t[i];
+  // }
+  // cout << '\n';
+
   for (int i = 0; i < len; ++i) {
-    if (t[i] == 'P') {
-      ok = true;
-    } else if (t[i] == '?') {
-      if (ok) {
-        t[i] = 'D';
-        ok = false;
-      } else {
-        t[i] = 'P';
-        ok = true;
-      }
-    } else {
-      ok = false;
+    if (t[i] == '?') {
+      t[i] = 'D';
     }
     cout << t[i];
   }
