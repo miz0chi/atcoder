5148284864180a948793bc933e184e8d9f9dd7bc
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-05-30 21:26:17 +0900

    submit arcnomura2020_b

---
+++ arc/nomura2020/b.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  string t;
+  cin >> t;
+  string pd = t, d = t;
+  int len = t.size();
+
+  bool ok = false;
+  for (int i = 0; i < len; ++i) {
+    if (t[i] == 'P') {
+      ok = true;
+    } else if (t[i] == '?') {
+      if (ok) {
+        t[i] = 'D';
+        ok = false;
+      } else {
+        t[i] = 'P';
+        ok = true;
+      }
+    } else {
+      ok = false;
+    }
+    cout << t[i];
+  }
+  cout << '\n';
+
+  return 0;
+}
