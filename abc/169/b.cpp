3c842589111377db9a7432355beae4f3e649a3d6
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-06-01 11:44:56 +0900

    submit abc169_b

--- abc/169/b.cpp
+++ abc/169/b.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+constexpr int MOD = 1000000007;
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -10,18 +12,21 @@ int main() {
   int n;
   cin >> n;
 
-  int64_t a;
-  cin >> a;
-  int64_t ans = a;
+  int64_t ans = 0;
   bool ok = true;
-  for (int i = 1; i < n; ++i) {
+  for (int i = 0; i < n; ++i) {
+    int64_t a;
     cin >> a;
     if (a == 0) {
       ans = 0;
       break;
     }
     if (ok) {
-      ans *= a;
+      if (i == 0) {
+        ans += a;
+      } else {
+        ans *= a;
+      }
       if (ans > 1000000000000000000) {
         ans = -1;
         ok = false;
