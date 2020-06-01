7cfd5d07feadfd787ccc189923bf9d09dd05fd27
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-06-01 12:04:26 +0900

    submit abc169_b

--- abc/169/b.cpp
+++ abc/169/b.cpp
@@ -1,11 +1,9 @@
 #include <bits/stdc++.h>
 
-#include <boost/multiprecision/cpp_int.hpp>
+// #include <boost/multiprecision/cpp_int.hpp>
 
 using namespace std;
-namespace mp = boost::multiprecision;
-
-constexpr int MOD = 1000000007;
+// namespace mp = boost::multiprecision;
 
 int main() {
   ios::sync_with_stdio(false);
@@ -15,24 +13,47 @@ int main() {
   int n;
   cin >> n;
 
-  mp::int128_t ans = 0;
+  // mp::int128_t ans = 0;
+  // bool ok = true;
+  // for (int i = 0; i < n; ++i) {
+  //   mp::int128_t a;
+  //   cin >> a;
+  //   if (a == 0) {
+  //     ans = 0;
+  //     break;
+  //   }
+  //   if (ok) {
+  //     if (i == 0) {
+  //       ans += a;
+  //     } else {
+  //       ans *= a;
+  //     }
+  //     if (ans > 1000000000000000000) {
+  //       ans = -1;
+  //       ok = false;
+  //     }
+  //   }
+  // }
+
+  int64_t ans = 0;
   bool ok = true;
   for (int i = 0; i < n; ++i) {
-    mp::int128_t a;
+    int64_t a;
     cin >> a;
     if (a == 0) {
       ans = 0;
       break;
     }
     if (ok) {
-      if (i == 0) {
-        ans += a;
-      } else {
-        ans *= a;
-      }
-      if (ans > 1000000000000000000) {
+      if (ans > 1000000000000000000 / a) {
         ans = -1;
         ok = false;
+      } else {
+        if (i == 0) {
+          ans += a;
+        } else {
+          ans *= a;
+        }
       }
     }
   }
