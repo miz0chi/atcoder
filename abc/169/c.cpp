58cbba21d405f8e0b7545c871f487e2cb1fc4a5d
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-06-01 12:39:56 +0900

    submit abc169_c

--- abc/169/c.cpp
+++ abc/169/c.cpp
@@ -1,19 +1,23 @@
 #include <bits/stdc++.h>
 
+#include <boost/multiprecision/cpp_int.hpp>
+
 using namespace std;
+namespace mp = boost::multiprecision;
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   cout << fixed << setprecision(15);
 
-  int64_t a;
-  double b;
+  long double a, b;
+  int64_t ans;
   cin >> a >> b;
 
   a *= b;
+  ans = (int64_t)a;
 
-  cout << a << '\n';
+  cout << ans << '\n';
 
   return 0;
 }
