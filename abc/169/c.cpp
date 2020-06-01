91bbc81dc2bf0076214fee7aea7f237fee282ed2
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-06-01 12:30:56 +0900

    submit abc169_c

---
+++ abc/169/c.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int64_t a;
+  double b;
+  cin >> a >> b;
+
+  a *= b;
+
+  cout << a << '\n';
+
+  return 0;
+}
