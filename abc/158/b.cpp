95ce4f970da5ed297402e060b95128a86e8bae8b
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-06-10 23:29:00 +0900

    submit abc158_b

---
+++ abc/158/b.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int64_t n, a, b;
+  cin >> n >> a >> b;
+
+  int64_t ans;
+
+  ans = (n * a / (a + b)) + (n % (a + b));
+
+  cout << ans << '\n';
+
+  return 0;
+}
