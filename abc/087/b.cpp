2444b91890d2e441822f657fcf476d4cfe244702
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-06-17 21:12:16 +0900

    submit abc087_b

--- abc/087/b.cpp
+++ abc/087/b.cpp
@@ -3,18 +3,19 @@
 using namespace std;
 
 int main() {
-  cin.tie(0);
   ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
 
   int A, B, C, X;
   cin >> A >> B >> C >> X;
 
   int sum = 0;
-  for (int i = 0; i < A + 1; i++) {
-    for (int j = 0; j < B + 1; j++) {
-      for (int k = 0; k < C + 1; k++) {
+  for (int i = 0; i < A + 1; ++i) {
+    for (int j = 0; j < B + 1; ++j) {
+      for (int k = 0; k < C + 1; ++k) {
         if (X == (i * 500 + j * 100 + k * 50)) {
-          sum++;
+          ++sum;
         }
       }
     }
