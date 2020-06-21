0b1519e757c8e800a7d7a6581318da4d74c5d7a7
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-06-21 19:36:53 +0900

    submit abc123_b

--- abc/123/b.cpp
+++ abc/123/b.cpp
@@ -8,24 +8,16 @@ int main() {
   cout << fixed << setprecision(15);
 
   vector<int> a(5);
-  int mi = 10, ans = 0;
+  int rem = 0, ans = 0;
   for (int i = 0; i < 5; ++i) {
     cin >> a[i];
-    if (a[i] == 0) {
-      ans += 10;
-    } else {
-      ans += ((a[i] - 1) / 10 + 1) * 10;
-    }
-    cerr << ans << '\n';
+    int aa = (a[i] + 9) / 10 * 10;
+    ans += aa;
+    // cerr << ans << '\n';
 
-    a[i] %= 10;
-    if (a[i] != 0) {
-      mi = min(mi, a[i]);
-    }
+    rem = max(rem, aa - a[i]);
   }
-  if (mi != 10) {
-    ans -= (10 - mi);
-  }
+  ans -= rem;
 
   cout << ans << '\n';
 
