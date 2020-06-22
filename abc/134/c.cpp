3cbe70b6b3dca9aa0cb9676cc58a750345454f3e
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-06-22 11:31:43 +0900

    submit abc134_c

--- abc/134/c.cpp
+++ abc/134/c.cpp
@@ -11,16 +11,14 @@ int main() {
   cin >> n;
   vector<int> a(n);
 
-  int mn = 0;
   for (int i = 0; i < n; ++i) {
     cin >> a[i];
-    mn = max(mn, a[i]);
   }
   vector<int> sa = a;
   sort(sa.begin(), sa.end(), greater<int>());
 
   for (int i = 0; i < n; ++i) {
-    if (a[i] == mn) {
+    if (a[i] == sa[0]) {
       cout << sa[1] << '\n';
     } else {
       cout << sa[0] << '\n';
