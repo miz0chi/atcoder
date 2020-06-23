d0ca580083c0b93f0d90d0b79565c0e3463b4354
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-06-23 12:19:45 +0900

    submit abc118_b

---
+++ abc/118/b.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int n, m;
+  cin >> n >> m;
+
+  vector<bool> ok(m, true);
+
+  for (int i = 0; i < n; ++i) {
+    int k;
+    cin >> k;
+    vector<bool> a(m, false);
+    for (int j = 0; j < k; ++j) {
+      int idx;
+      cin >> idx;
+      a[idx - 1] = true;
+    }
+    for (int j = 0; j < m; ++j) {
+      ok[j] = ok[j] && a[j];
+      // cerr << ok[j] << " ";
+    }
+    // cerr << '\n';
+  }
+
+  int cnt = 0;
+  for (int i = 0; i < m; ++i) {
+    if (ok[i]) {
+      ++cnt;
+    }
+  }
+
+  cout << cnt << '\n';
+
+  return 0;
+}
