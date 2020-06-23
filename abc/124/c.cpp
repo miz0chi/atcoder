66200aae6b48c6fc6534ddff587db83af1053d05
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-06-23 22:48:57 +0900

    submit abc124_c

---
+++ abc/124/c.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  string s;
+  cin >> s;
+  int si = s.size();
+
+  int cnt1 = 0, cnt2 = 0;
+  for (int i = 0; i < si; ++i) {
+    if (i % 2 == 0) {
+      if (s[i] == '0') {
+        ++cnt1;
+      }
+    } else {
+      if (s[i] == '1') {
+        ++cnt1;
+      }
+    }
+    if (i % 2 == 1) {
+      if (s[i] == '0') {
+        ++cnt2;
+      }
+    } else {
+      if (s[i] == '1') {
+        ++cnt2;
+      }
+    }
+  }
+
+  cout << min(cnt1, cnt2) << '\n';
+
+  return 0;
+}
