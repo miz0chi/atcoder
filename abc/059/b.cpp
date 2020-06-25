a4ebfa63437e7d8b56d25d823a966cc547908428
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-06-25 12:20:07 +0900

    submit abc059_b

---
+++ abc/059/b.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  string a, b;
+  cin >> a >> b;
+
+  string ans;
+  if (a.size() > b.size()) {
+    ans = "GREATER";
+  } else if (a.size() < b.size()) {
+    ans = "LESS";
+  } else {
+    ans = "EQUAL";
+    for (unsigned int i = 0; i < a.size(); ++i) {
+      if (a[i] > b[i]) {
+        ans = "GREATER";
+      } else if (a[i] < b[i]) {
+        ans = "LESS";
+      }
+    }
+  }
+
+  cout << ans << '\n';
+
+  return 0;
+}
