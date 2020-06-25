9ace5c24d2183dd2da1ad83423122ceea09cf082
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-06-25 13:12:06 +0900

    submit abc083_b

--- abc/083/b.cpp
+++ abc/083/b.cpp
@@ -2,28 +2,6 @@
 
 using namespace std;
 
-#define deg_to_rad(deg) (((deg) / 360) * 2 * M_PI)
-#define rad_to_deg(rad) (((rad) / 2 / M_PI) * 360)
-
-typedef long long ll;
-typedef long double ld;
-
-typedef vector<int> vi;
-typedef vector<ld> vd;
-typedef vector<ll> vl;
-typedef vector<string> vs;
-
-typedef vector<vector<int>> vvi;
-typedef vector<vector<ld>> vvd;
-typedef vector<vector<ll>> vvl;
-
-typedef vector<vector<vector<int>>> vvvi;
-typedef vector<vector<vector<ld>>> vvvd;
-typedef vector<vector<vector<ll>>> vvvl;
-
-constexpr int MOD = 1000000007;
-constexpr char nl = '\n';
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -32,8 +10,8 @@ int main() {
   int N, A, B;
   cin >> N >> A >> B;
 
-  ll ans = 0LL;
-  for (int i = 1; i <= N; i++) {
+  int64_t ans = 0;
+  for (int i = 1; i <= N; ++i) {
     int n = i;
     int sum = 0;
     while (n > 0) {
@@ -41,9 +19,8 @@ int main() {
       n /= 10;
     }
     if (A <= sum && sum <= B) {
-      ans += i;//
-      // cout << sum << ":";
+      ans += i;
     }
   }
-  cout << ans << endl;
+  cout << ans << '\n';
 }
