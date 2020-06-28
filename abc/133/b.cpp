75d1e11d30052de910377672b9ccae702617a8ea
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-06-28 17:15:59 +0900

    submit abc133_b

---
+++ abc/133/b.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout << fixed << setprecision(15);
+
+  int n, d;
+  cin >> n >> d;
+
+  vector<vector<int>> x(n, vector<int>(d));
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < d; ++j) {
+      cin >> x[i][j];
+    }
+  }
+
+  int cnt = 0;
+  for (int i = 0; i < n - 1; ++i) {
+    for (int j = i + 1; j < n; ++j) {
+      double dist = 0.0;
+      for (int k = 0; k < d; ++k) {
+        dist += (x[i][k] - x[j][k]) * (x[i][k] - x[j][k]);
+      }
+      dist = sqrt(dist);
+      if (floor(dist) == dist) {
+        ++cnt;
+      }
+    }
+  }
+
+  cout << cnt << '\n';
+
+  return 0;
+}
