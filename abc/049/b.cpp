166084c6e855eab12764c3bbdc4d14a0046ee0e5
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-07-03 12:56:54 +0900

    submit abc049_b

---
+++ abc/049/b.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int h, w;
+  cin >> h >> w;
+
+  for (int i = 0; i < h; ++i) {
+    string s;
+    cin >> s;
+    cout << s << '\n';
+    cout << s << '\n';
+  }
+
+  return 0;
+}
