24e5700e4a424db03110775f5755d7d4712c0fa3
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-07-06 17:08:36 +0900

    submit abc128_b

--- abc/128/b.cpp
+++ abc/128/b.cpp
@@ -12,12 +12,10 @@ int main() {
   vector<tuple<string, int, int>> p(n);
   for (int i = 0; i < n; ++i) {
     cin >> get<0>(p[i]) >> get<1>(p[i]);
+    get<1>(p[i]) *= -1;
     get<2>(p[i]) = i + 1;
   }
-  sort(begin(p), end(p), [](const auto &x, const auto &y) {
-    return get<0>(x) == get<0>(y) ? get<1>(x) > get<1>(y)
-                                  : get<0>(x) < get<0>(y);
-  });
+  sort(begin(p), end(p));
 
   for (int i = 0; i < n; ++i) {
     cout << get<2>(p[i]) << '\n';
