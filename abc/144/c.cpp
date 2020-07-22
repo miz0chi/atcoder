1b27d3e1cddaa6607950892b14dfc5c927552fac
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-07-22 19:56:40 +0900

    submit abc144_c

---
+++ abc/144/c.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int64_t n;
+  cin >> n;
+
+  int64_t rtn = sqrt(n);
+  int64_t ans = 0;
+  for (int i = 1; i < rtn; ++i) {
+    if (n % i == 0) {
+      ans = n / i + i - 2;
+    }
+  }
+
+  cout << ans << '\n';
+
+  return 0;
+}
