1be1924b2650859b9dae1905cf047053d12d019a
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-08-01 20:11:35 +0900

    submit abc106_c

---
+++ abc/106/c.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string s;
+  int64_t k;
+  cin >> s >> k;
+
+  int ss = s.size();
+
+  char ans = '1';
+  for (int i = 0; i < ss; ++i) {
+    if (s[i] != '1') {
+      ans = s[i];
+      break;
+    }
+  }
+
+  cout << ans << '\n';
+
+  return 0;
+}
