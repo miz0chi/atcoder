8c8995e4f03bcf7d285d44b129c223d89682bcd6
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-08-15 21:53:15 +0900

    submit abc175_b

---
+++ abc/175/b.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  vector<int64_t> l(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> l[i];
+  }
+  sort(l.begin(), l.end());
+
+  int ans = 0;
+  for (int i = 0; i < n - 2; ++i) {
+    for (int j = i + 1; j < n - 1; ++j) {
+      for (int k = j + 1; k < n; ++k) {
+        if (l[i] != l[j] && l[j] != l[k] && l[i] + l[j] > l[k]) {
+          ++ans;
+        }
+      }
+    }
+  }
+
+  cout << ans << '\n';
+
+  return 0;
+}
