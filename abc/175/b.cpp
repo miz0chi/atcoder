3d519158b50171ac4e5a9f03e839783b3b31ae61
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-08-16 09:45:07 +0900

    submit abc175_b

--- abc/175/b.cpp
+++ abc/175/b.cpp
@@ -18,8 +18,11 @@ int main() {
   int ans = 0;
   for (int i = 0; i < n - 2; ++i) {
     for (int j = i + 1; j < n - 1; ++j) {
+      if (l[i] == l[j]) {
+        continue;
+      }
       for (int k = j + 1; k < n; ++k) {
-        if (l[i] != l[j] && l[j] != l[k] && l[i] + l[j] > l[k]) {
+        if (l[j] != l[k] && l[i] + l[j] > l[k]) {
           ++ans;
         }
       }
