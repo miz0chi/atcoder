83be6d9a6becf5df93e783339d2ae1210b100e46
miz0chi <b6701495@planet.kanazawa-it.ac.jp>
2020-08-29 22:20:03 +0900

    submit abc177_c

--- abc/177/c.cpp
+++ abc/177/c.cpp
@@ -10,19 +10,22 @@ int main() {
   int n;
   cin >> n;
 
-  int64_t nsum = 0;
-
   vector<int64_t> a(n);
+  vector<int64_t> am(n);
   for (int i = 0; i < n; ++i) {
     cin >> a[i];
-    nsum += a[i];
-    nsum %= MOD;
+  }
+  am[0] = a[n-1];
+  am[0] %= MOD;
+  for (int i = 1; i < n; ++i) {
+      am[i] = am[i-1] + a[n-i-1];
+      am[i] %= MOD;
   }
 
   int64_t ans = 0;
 
-  for (int i = 0; i < n; ++i) {
-    ans += ((nsum - a[i]) * a[i]);
+  for (int i = 0; i < n-1; ++i) {
+    ans += (am[n-i-2] * a[i]);
     ans %= MOD;
   }
 
